20cf2441a4f36b7c32d35e6392397d9b5a386438
TossedBloom <tossedbloom@outlook.com>
2024-07-15 23:09:47 +0800

    update vue router

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,25 +6,36 @@ const routes: Array<RouteRecordRaw> = [
     path: "/",
     name: "home",
     component: HomeView,
-    meta: { hideNavbar: true },
+    meta: { 
+      hideNavbar: true,
+      title: 'TossedBloom Studios'
+     },
   },
   {
     path: "/projects",
     name: "project",
+    meta: { 
+      hideNavbar: true,
+      title: 'Projects'
+    },
     component: () => import("../views/ProjectsView.vue")
   },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
+  // {
+  //   path: "/about",
+  //   name: "about",
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () =>
+  //     import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  // },
   {
     path: "/potatodex",
     name: "potato-dex",
+    meta: { 
+      hideNavbar: true,
+      title: 'PotatoDex - Potato Almanac'
+    },
     component: () => 
       import("../components/project/tatodex/site/Index.vue"),
   }
