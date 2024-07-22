1508022ac8144cb3e1f8cda4f1762dd30dd6de37
TossedBloom <tossedbloom@outlook.com>
2024-07-22 08:59:04 +0800

    svg and webp declarations

--- src/images.d.ts
+++ src/images.d.ts
@@ -4,6 +4,16 @@ declare module '*.png' {
     export default value;
 }
 
+declare module '*.svg' {
+    const value: string;
+    export default value;
+}
+
+declare module '*.webp' {
+    const value: string;
+    export default value;
+}
+
 // src/videos.d.ts
 declare module '*.mp4' {
     const src: string;
